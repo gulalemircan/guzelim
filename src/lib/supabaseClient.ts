4b284f0c8cd266f816245cddcd543dbcf0547eb7
gulalemircan <gulalemircan@gmail.com>
2026-07-15 15:08:22 +0300

    supabase url düzeltildi

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Vercel ayarlarıyla uğraşmamak için linki ve şifreyi direkt buraya yazıyoruz
-const supabaseUrl = 'https://rhruclubaprrspwgbcms.supabase.co/rest/v1/';
+// DÜZELTME: Linkin sonundaki /rest/v1/ kısmı silindi, sadece ana domain bırakıldı!
+const supabaseUrl = 'https://rhruclubaprrspwgbcms.supabase.co';
 const supabaseAnonKey = 'sb_publishable_cqsPM1BzDex-SYjsxnbM4g_P5NjhXAf';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
